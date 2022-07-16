30ba214ab293a73fba2f4284b49ee027589631ae
brendonlh <brendonhampton1988@gmail.com>
2022-07-16 14:42:44 -0700

    added a second scraper for american civlil war

--- scraper_two.py
+++ scraper_two.py
@@ -6,15 +6,13 @@ url = 'https://en.wikipedia.org/wiki/American_Civil_War'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 prettier_soup = soup.findAll(text='citation needed')
-single_parent = prettier_soup[0]
-soup_parents = single_parent.findParent('p')
 
-# for item in soup_parents:
-#     print(item)
+for text in prettier_soup:
+    soup_text = text.findParent('p')
+    soup_text = soup_text.text.split()
+    string_text = ' '.join(soup_text)
+    print(string_text)
+    print('-------------------------------------------------------------------------')
 
-soup_text = soup_parents.text.split()
-
-string_text = ' '.join(soup_text)
-print(string_text)
 citations_count = len(prettier_soup)
 print(f'{citations_count} citations needed')
